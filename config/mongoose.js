ef373ae2af411622bfd3cdee2d1cabf7df85aa8e
sayamjn <sayamjain0708@gmail.com>
2023-09-21 22:08:35 +0530

    final2

--- config/mongoose.js
+++ config/mongoose.js
@@ -1,20 +1,25 @@
+// Import necessary libraries and configuration
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 dotenv.config();
 
-// Replace 'your_connection_string_here' with your actual MongoDB connection string
+// Get the MongoDB connection string from environment variables
 const uri = process.env.MONGODB_LOCAL_SERVER_URL;
 
+// Connect to MongoDB using Mongoose
 mongoose.connect(uri, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
 
+// Get the default connection
 const db = mongoose.connection;
 
+// Set up event listeners
 db.on('error', console.error.bind(console, 'Connection error:'));
 db.once('open', () => {
   console.log('Connected to MongoDB database');
 });
 
+// Export the database connection for use in other parts of your application
 export default db;
